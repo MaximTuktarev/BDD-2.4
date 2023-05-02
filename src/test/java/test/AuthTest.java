f36ef9c574112b0f6dd1b2a35d2f45d025c6353b
MaximTuktarev <maxim.tuktarev@mail.ru>
2023-05-02 18:16:13 +0400

    little_fixes

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -1,13 +1,11 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import Data.DataHelper;
 import Page.LoginPage;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
