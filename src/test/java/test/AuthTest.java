1ddc630d84cbaeab8dbca418f4110bf107243291
MaximTuktarev <maxim.tuktarev@mail.ru>
2023-05-10 21:06:26 +0400

    chrome_update

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -3,8 +3,11 @@ package test;
 import Data.DataHelper;
 import Page.LoginPage;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static Data.DataHelper.getFirstCardInfo;
 import static Data.DataHelper.getSecondCardInfo;
@@ -13,10 +16,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class MoneyTransferTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void SetUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
 
     @BeforeEach
     void SetUp() {
         Configuration.holdBrowserOpen = true;
+        
     }
 
     @Test
